88154b961cc4eedde8c67ee8a2f736ef7cea14ac
Atila Neves <atila.neves@gmail.com>
2019-03-02 14:33:52 +0100

    ut.spreadsheet.dynamic

--- tests/ut/spreadsheet.d
+++ tests/ut/spreadsheet.d
@@ -4,7 +4,7 @@ module ut.spreadsheet;
 import ut;
 
 
-@("paper")
+@("static")
 @safe pure unittest {
 
     import std.typecons: nullable;
@@ -30,4 +30,40 @@ import ut;
     b.build("B2");
     // Should have built B1 as part of building B2
     store.should == [ "A1": 10, "A2": 20, "B1": 30, "B2": 60];
+}
+
+
+@("dynamic")
+@safe pure unittest {
+
+    import std.typecons: nullable;
+
+    static MaybeTask!int formulae(F)(in string cellName, F fetch) {
+        switch(cellName) {
+
+        default:  // leaf node
+            return typeof(return).init;
+
+        case "B1": // B1: IF(C1=1,B2,A2)
+            return nullable(() => fetch("C1") == 1 ? fetch("B2") : fetch("A2"));
+
+        case "B2": // B2: IF(C1=1,A1,B1)
+            return nullable(() => fetch("C1") == 1 ? fetch("A1") : fetch("B1"));
+        }
+    }
+
+    auto store = ["A1": 10, "A2": 20, "C1": 1];
+    auto b = busy!formulae(store);
+    store.should == ["A1": 10, "A2": 20, "C1": 1];  // nothing happened yet
+
+    b.build("B2");
+    store.should == [ "A1": 10, "A2": 20, "B2": 10, "C1": 1];
+
+    b.build("B1");
+    store.should == [ "A1": 10, "A2": 20, "B1": 10, "B2": 10, "C1": 1];
+
+    store = ["A1": 30, "A2": 40, "C1": 0];
+    b.build("B2");
+    // Should build B1 as part of building B2
+    store.should == [ "A1": 30, "A2": 40, "B1": 40, "B2": 40, "C1": 0];
 }
